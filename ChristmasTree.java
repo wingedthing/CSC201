8df43f8b612f9502439c7bc2c9276cfb0a40f881
wingedthing <46513743+wingedthing@users.noreply.github.com>
2020-10-09 15:10:18 -0400

    Create ChristmasTree.java
    Prints a tree based on user input base size, this was for a project on mooc.fi

---
+++ ChristmasTree.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+public class ChristmasTree {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the size of your tree >> ");
+		int numStars = Integer.valueOf(scan.nextLine());
+		
+		//printTriangle(5);
+		//System.out.println("---");
+		//christmasTree(3);
+		//System.out.println("---");
+		christmasTree(numStars);
+		scan.close();
+	}
+
+	public static void printStars(int number) {
+		while (number > 0) {
+			System.out.print("*");
+			number--;
+		}
+		
+		System.out.println("");
+	}
+
+	public static void printSpaces(int number) {
+		while (number > 0) {
+			System.out.print(" ");
+			number--;
+		}
+
+	}
+
+	public static void printTriangle(int size) {
+		int stars = 1;
+		int spaces = size - 1;
+		while (stars <= size) {
+			printSpaces(spaces);
+			printStars(stars);
+			stars++;
+			spaces--;
+		}
+
+	}
+
+	public static void christmasTree(int height) {
+		int space = height - 1;
+		int star = 1;
+		if (height == 1) {
+			printSpaces(1);
+			printStars(1);
+		} else {
+			while (space >= 0) {
+				printSpaces(space);
+				printStars(star);
+				star += 2;
+				space--;
+
+			}
+		}
+		
+		for (int i = 0; i < 2; i++) {
+			printSpaces(height - 2);
+			printStars(3);
+		}
+
+	}
+
+}
